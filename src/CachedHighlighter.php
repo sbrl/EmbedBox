b489afb9a32a792d9651e33cc176905ae64138cf
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2020-07-04 15:59:41 +0100

    Finish CachedHighlighter implementation

--- src/CachedHighlighter.php
+++ src/CachedHighlighter.php
@@ -11,31 +11,74 @@ class CachedHighlighter {
 	function __construct(TomlSettings $settings, Stash\Pool $cache, Highlighter $highlighter) {
 		$this->settings = $settings;
 		$this->cache = $cache;
+		$this->highlighter = $highlighter;
 	}
 	
-	public function highlight_url($url, $do_cache = true) {
-		$cache_key = hash("sha256", $url);
+	public function highlight_url(string $url, string $type, $do_cache = true) {
+		// 0: Setup and validation
+		if($type == "__AUTO__")
+			$type = $this->get_file_ext($url);
+		if($type == null)
+			return [ "success" => false, "error" => "autodetect_failed" ];
+		
+		$cache_key = $this->get_cache_key($url, $type);
+		
 		
 		// 1: Check the cache
 		$item = $this->cache->getItem("render_url/$cache_key");
 		if($item->isHit() && $do_cache)
-			return (object) [ "result" => $item->get(), "was_hit" => true ];
+			return (object) [
+				"success" => true,
+				"result" => $item->get(),
+				"was_hit" => true
+			];
+		
 		
 		// 2: Render if it's not in the cache
 		$item->lock(); // Let others know we're updating the cache
 		$result = $this->do_highlight_string(
-			
+			$type,
 			file_get_contents($url)
 		);
+		
 		
 		// 3: Update the cache
 		$this->cache->save($item->set($result));
 		
+		
 		// 4: Return result
-		return (object) [ "result" => $result, "was_hit" => false ];
+		return (object) [
+			"success" => true,
+			"result" => $result,
+			"was_hit" => false
+		];
+	}
+	
+	private function get_cache_key(string $url, string $type) : string {
+		return hash("sha256", 
+			hash("sha256", $url)."|".hash("sha256", $type)
+		);
+	}
+	
+	private function get_file_ext(string $url) {
+		$filename = basename($url);
+		$pos = strpos($filename, ".");
+		if($pos === false)
+			return null;
+		
+		return substr($filename, $pos + 1);
 	}
 	
 	private function do_highlight_string($type, $source) {
-		
+		$result = null;
+		try {
+			$result = $this->highlighter->highlight($type, $source);
+		} catch(DomainException $error) {
+			$result = (object) [
+				"language" => "unknown-language",
+				"value" => htmlentities($source)
+			];
+		}
+		return "<pre><code class='hljs {$result->language}'>$result->value</code></pre>";
 	}
 }
