83e0d4610ba27d88f2f30939dc165b0b31ea4a8c
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2020-07-05 01:54:17 +0100

    Style the embed

--- src/Actions/StylesheetList.php
+++ src/Actions/StylesheetList.php
@@ -17,6 +17,7 @@ class StylesheetList
 		$format = $_GET["format"] ?? "text";
 		
 		$themes = \HighlightUtilities\getAvailableStyleSheets();
+		sort($themes);
 		
 		$mime = "text/plain";
 		$result = null;
